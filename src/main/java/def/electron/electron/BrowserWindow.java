dbac8dbdc675701c6a24333a787b0b8aaf7bbad8
Aleksandar Ivanov <smichess@gmail.com>
2017-07-27 15:11:55 +0300

    updating window events to accept runnable

--- src/main/java/def/electron/electron/BrowserWindow.java
+++ src/main/java/def/electron/electron/BrowserWindow.java
@@ -3,6 +3,7 @@ import def.js.Function;
 public class BrowserWindow extends def.js.Object {
     public BrowserWindow(BrowserWindowOptions options){}
     native public BrowserWindow on(String event, Function listener);
+    native public BrowserWindow on(String event, Runnable listener);
     public WebContents webContents;
     native public Object loadURL(String url);
     native public Object setMenu(Object menu);
