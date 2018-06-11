d7a196146cfd82fede8dc9884d52ac5914e83b8a
Ville Venäläinen <ville@mindhive.fi>
2018-06-11 12:39:46 +0300

    size and position methods for BrowserWindow + some minor code formatting

--- src/main/java/def/electron/electron/BrowserWindow.java
+++ src/main/java/def/electron/electron/BrowserWindow.java
@@ -1,21 +1,79 @@
 package def.electron.electron;
-import def.js.Function;
-public class BrowserWindow extends def.js.Object {
-    public BrowserWindow(BrowserWindowOptions options){}
-    native public BrowserWindow on(String event, Function listener);
-    native public BrowserWindow on(String event, Runnable listener);
-    public WebContents webContents;
-    native public Object loadURL(String url);
-    native public Object setMenu(Object menu);
-    native public void maximize();
-    native public void minimize();
-    native public void close();
-    native public void show();
-    native public void blur();
-    native public void restore();
-    native public void center();
-    native public void setKiosk(boolean isKiosk);
-    native public boolean isKiosk();
-    public BrowserWindow(){}
-}
 
+import def.js.Function;
+
+public class BrowserWindow extends def.js.Object {
+
+	public WebContents webContents;
+
+	public BrowserWindow() {
+	}
+
+	public BrowserWindow(BrowserWindowOptions options) {
+	}
+
+	public native BrowserWindow on(String event, Function listener);
+
+	public native BrowserWindow on(String event, Runnable listener);
+
+	public native Object loadURL(String url);
+
+	public native Object setMenu(Object menu);
+
+	public native void maximize();
+
+	public native void minimize();
+
+	public native void close();
+
+	public native void show();
+
+	public native void blur();
+
+	public native void restore();
+
+	public native void center();
+
+	public native void setKiosk(boolean isKiosk);
+
+	public native boolean isKiosk();
+
+	/**
+	 * Returns Integer[] - Contains the window's current position.
+	 *
+	 * @return Current x, y -position of window
+	 */
+	public native def.js.Array<Integer> getPosition();
+
+	/**
+	 * Moves window to x and y.
+	 *
+	 * @param x
+	 *            Integer
+	 *
+	 * @param y
+	 *            Integer
+	 * @param animate
+	 *            Boolean (optional) macOS
+	 */
+	public native void setPosition(Integer x, Integer y, Boolean animate);
+
+	/**
+	 * Returns Integer[] - Contains the window's width and height.
+	 *
+	 * @return Current x, y -position of window
+	 */
+	public native def.js.Array<Integer> getSize();
+
+	/**
+	 * Set window's width and height.
+	 *
+	 * @param width
+	 *            New window width
+	 * @param height
+	 *            New window height
+	 * @param animate
+	 *            Boolean (optional) macOS
+	 */
+	public native void setSize(int width, int height, boolean animate);
+}
--- src/main/java/def/electron/electron/WebContents.java
+++ src/main/java/def/electron/electron/WebContents.java
@@ -1,11 +1,31 @@
 package def.electron.electron;
+
 @jsweet.lang.Interface
 public abstract class WebContents extends def.js.Object {
-    native public Object openDevTools();
-    native public WebContents on(String event, Runnable listener);
-    native public WebContents on(String event, NewWindowCallback<Event, String, String, Object, BrowserWindowOptions, Object> callback);
-    native public Object send(String pipe, def.js.Object data);
-    native public void printToPDF(PrintOptions options, java.util.function.BiConsumer<?, ?> callback);
-    native public void print();
-}
 
+	/**
+	 * A Integer representing the unique ID of this WebContents.
+	 */
+	public int id;
+
+	public native Object openDevTools();
+
+	public native WebContents on(String event, Runnable listener);
+
+	public native WebContents on(String event,
+			NewWindowCallback<Event, String, String, Object, BrowserWindowOptions, Object> callback);
+
+	public native Object send(String pipe, def.js.Object data);
+
+	public native void printToPDF(PrintOptions options, java.util.function.BiConsumer<?, ?> callback);
+
+	public native void print();
+
+	/**
+	 * The pid of the associated renderer process.
+	 *
+	 * @return pid of associated renderer process.
+	 */
+	public native int getOSProcessId();
+
+}
